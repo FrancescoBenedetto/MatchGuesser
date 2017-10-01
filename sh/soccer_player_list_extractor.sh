20568d26528ea0ef800cb1a7933f2f90252e7f80
FrancescoBenedetto <fra.benedetto.91@gmail.com>
2017-10-01 15:48:36 +0200

    list of players from '10-'17 crawled.

--- sh/soccer_player_list_extractor.sh
+++ sh/soccer_player_list_extractor.sh
@@ -1,9 +1,11 @@
+year=$1
+max_pages=$2
+seriea_name=$3
 
-max_pages=22
-location_dir=~/workspace/MatchGuesser/webpages/13-seriea/
-base_page=http://www.futhead.com/13/leagues/seriea/?page=
+location_dir=~/workspace/MatchGuesser/webpages/$year-seriea/
+base_page=http://www.futhead.com/$year/leagues/$seriea_name/?page=
 
-for i in $(seq 1 $max_pages); 
+for i in $(seq 1 $max_pages);
 do wget -P $location_dir $base_page$i;
 
 done
