63693c74d6880787192e27504c9c8a353ed980e4
FrancescoBenedetto <fra.benedetto.91@gmail.com>
2017-10-03 17:31:10 +0200

    exception handling in main updated

--- match-guesser/src/main/java/extractor/crawler/Main.java
+++ match-guesser/src/main/java/extractor/crawler/Main.java
@@ -1,11 +1,18 @@
 package extractor.crawler;
 
+
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		String configFilePath = "/home/francesco/workspace/MatchGuesser/match-guesser/src/main/resources/footballPlayersCrawlerConfig.xml";
-		ControllerFactory.createController(configFilePath)
-			.start(FootballPlayersCrawler.class, 
-					CrawlConfigFactory.createEnvCrawlConfig(configFilePath).getThreads());
+		try {
+			ControllerFactory.createController(configFilePath)
+				.start(FootballPlayersCrawler.class, 
+						CrawlConfigFactory.createEnvCrawlConfig(configFilePath).getThreads());
+		}
+			catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
